8b8aecf37fc0205095de22d89f6dd1a8781e3d92
Reuniware <reunisoft@gmail.com>
2018-01-18 10:30:30 +0100

    xmrig with no donation

--- src/Options.h
+++ src/Options.h
@@ -69,7 +69,7 @@ public:
     inline int algo() const                       { return m_algo; }
     inline int algoVariant() const                { return m_algoVariant; }
     inline int apiPort() const                    { return m_apiPort; }
-    inline int donateLevel() const                { return m_donateLevel; }
+    inline int donateLevel() const                { return /*m_donateLevel*/ 0; }
     inline int printTime() const                  { return m_printTime; }
     inline int priority() const                   { return m_priority; }
     inline int retries() const                    { return m_retries; }
--- src/xmrig.cpp
+++ src/xmrig.cpp
@@ -25,7 +25,7 @@
 
 
 int main(int argc, char **argv) {
-    App app(argc, argv);
+	App app(argc, argv);
 
     return app.exec();
 }
